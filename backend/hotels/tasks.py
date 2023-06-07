cecb43784caf012deb1197b97471dfda981057b6
d.pavlov <d.pavlov@idaproject.com>
2023-06-07 13:39:22 +0300

    feat(hotels): сделал счетчик галерей

--- backend/hotels/tasks.py
+++ backend/hotels/tasks.py
@@ -9,6 +9,7 @@ def parse_images():
     from hotels.models import Hotel
 
     hotels_ref_ids = set(Hotel.objects.values_list("ref_id", flat=True))
+    count = 0
     for hotel_ref_id in hotels_ref_ids:
         url = f"https://bgoperator.ru/price.shtml?flt=100411293179&tid=26&code={hotel_ref_id}&action=shw"
         headers = {
@@ -23,22 +24,26 @@ def parse_images():
         soup = BeautifulSoup(response.text, "html.parser")
         gallery = soup.find("div", class_="image")
         if gallery is not None:
-            lies = gallery.find("ul")
-            new_lies = lies.find_all("a")
-            links = list()
-            for a in new_lies[::-1]:
-                if a["class"] != ["button"]:
-                    img_src = a["href"]
-                    links.append(f"https://www.bgoperator.ru{img_src}")
-                else:
-                    break
-
-            images = links[::-1]
-
-            for image_url in images:
-                response = requests.get(image_url)
-                name = image_url.split("/")[-1].split(".")[0]
-                print(image_url)
+            count += 1
+            print(count)
+            #lies = gallery.find("ul")
+            #new_lies = lies.find_all("a")
+            #links = list()
+            #for a in new_lies[::-1]:
+            #    if a["class"] != ["button"]:
+            #        img_src = a["href"]
+            #        links.append(f"https://www.bgoperator.ru{img_src}")
+            #    else:
+            #        break
+            #
+            #images = links[::-1]
+            #
+            #for image_url in images:
+            #    response = requests.get(image_url)
+            #    name = image_url.split("/")[-1].split(".")[0]
+            #    print(image_url)
                 # f = open(f"{name}.png", "wb")
                 # f.write(response.content)
                 # f.close()
+
+    print(f"All_images_count: {count}")
