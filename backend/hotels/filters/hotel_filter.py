c964103fd8eda11b160eaef084c7e023a2ba43af
d.pavlov <d.pavlov@idaproject.com>
2023-06-16 19:15:27 +0300

    feat(hotels): добавил фильтры для отелей

--- backend/hotels/filters/hotel_filter.py
+++ backend/hotels/filters/hotel_filter.py
@@ -1,4 +1,5 @@
-from common.filters import FacetFilterSet, CharInFilter
+from common.filters import FacetFilterSet, CharInFilter, NumberInFilter
+from django_filters.rest_framework import RangeFilter, BooleanFilter, Filter
 from hotels.models import Hotel
 
 
@@ -14,6 +15,14 @@ class HotelFilter(FacetFilterSet):
     resort = CharInFilter(field_name="resort__slug")
     resort.specs = "get_resort_specs"
     resort.aggregate = "resort_aggregate"
+    rate = RangeFilter()
+    popularity_level = NumberInFilter()
+    popularity_level.specs = "get_popularity_level_specs"
+    popularity_level.aggregate = "popularity_level_aggregate"
+    is_in_bonus_program = BooleanFilter()
+    category = CharInFilter(field_name="category__name")
+    category.specs = "get_category_specs"
+    category.aggregate = "category_aggregate"
 
     class Meta:
         model = Hotel
@@ -58,4 +67,57 @@ class HotelFilter(FacetFilterSet):
             queryset.values_list("resort__name", "resort__slug", flat=True)
             .order_by("resort__name", "resort__slug")
             .distinct("resort__name", "resort__slug")
+        )
+
+    @staticmethod
+    def get_popularity_level_specs(queryset):
+        levels = (
+            queryset
+            .order_by("popularity_level")
+            .distinct("popularity_level")
+            .values_list("popularity_level", flat=True)
+        )
+        popularity_level_map = {
+            0: "Низкая популярность",
+            1: "Средняя популярность",
+            2: "Высокая популярность"
+        }
+        return [{"value": level, "label": popularity_level_map.get(level, "Средняя популярность")} for level in levels]
+
+    @staticmethod
+    def popularity_level_aggregate(queryset):
+        return (
+            queryset.values_list("popularity_level", flat=True)
+            .order_by("popularity_level")
+            .distinct("popularity_level")
+        )
+
+    @staticmethod
+    def get_category_specs(queryset):
+        categories = (
+            queryset
+            .order_by("category__name")
+            .distinct("category__name")
+            .values_list("category__name", flat=True)
+        )
+        category_map = {
+            "HV-2": "Клубный отель 4-5 звезд",
+            "HV-1": "Клубный отель 3-4 звезд",
+            "5*": "5 звезд",
+            "Без звезд": "Без звезд",
+            "Villas": "Вилла",
+            "Apts": "Апартаменты",
+            "4*": "4 звезды",
+            "3*": "3 звезды",
+            "2*": "2 звезды",
+            "1*": "1 звезда",
+        }
+        return [{"value": category, "label": category_map.get(category, "Без звезд")} for category in categories]
+
+    @staticmethod
+    def category_aggregate(queryset):
+        return (
+            queryset.values_list("category__name", flat=True)
+            .order_by("category__name")
+            .distinct("category__name")
         )
